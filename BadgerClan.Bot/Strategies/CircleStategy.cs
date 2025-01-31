2237faaa847332f6f489321f0403bcd4f577cf93
Christopher Young <c.t.ythegamer@gmail.com>
2025-01-30 18:38:55 -0700

    more work on circle strat

--- BadgerClan.Bot/Strategies/CircleStategy.cs
+++ BadgerClan.Bot/Strategies/CircleStategy.cs
@@ -1,4 +1,6 @@
 ﻿using BadgerClan.Logic;
+using BadgerClan.Logic.Bot;
+using System.Drawing;
 
 namespace BadgerClan.Bot.Strategies
 {
@@ -10,10 +12,14 @@ namespace BadgerClan.Bot.Strategies
         public List<Move> GenerateMoves(MoveRequest request)
         {
             MyTeam = request.Units.Where(u => u.Team == request.YourTeamId);
-            if (ClosestEnemies == null)
-                GetClosestEnemies(request);
+            List<Move> moves = new List<Move>();
+            GetClosestEnemies(request);
 
-            return new List<Move>();
+            if (ClosestEnemies.First().Location.Distance(MyTeam.First().Location) < 6)
+                moves = SplitUp(moves);
+            else moves = MoveTowardsEnemy(moves);
+
+            return moves;
         }
 
         private void GetEnemyDirection()
@@ -52,7 +58,7 @@ namespace BadgerClan.Bot.Strategies
             }
         }
         
-        public void SplitUp(List<Move> moves)
+        public List<Move> SplitUp(List<Move> moves)
         {
             int firstHalf = MyTeam.Count() / 2;
             var teamArray = MyTeam.ToArray();
@@ -70,13 +76,43 @@ namespace BadgerClan.Bot.Strategies
                         Direction.NorthWest => teamArray[i].Location.MoveNorthEast(1),
                         _ => teamArray[i].Location
                     };
+                    moves.Add(new Move(MoveType.Walk, teamArray[i].Id, coord));
+                }
+                else
+                {
+                    var coord = ClosestEnemyDirection switch
+                    {
+                        Direction.NorthEast => teamArray[i].Location.MoveEast(1),
+                        Direction.East => teamArray[i].Location.MoveSouthEast(1),
+                        Direction.SouthEast => teamArray[i].Location.MoveSouthWest(1),
+                        Direction.SouthWest => teamArray[i].Location.MoveSouthEast(1),
+                        Direction.West => teamArray[i].Location.MoveSouthWest(1),
+                        Direction.NorthWest => teamArray[i].Location.MoveWest(1),
+                        _ => teamArray[i].Location
+                    };
+                    moves.Add(new Move(MoveType.Walk, teamArray[i].Id, coord));
                 }
             }
+            return moves;
         }
 
-        public void MoveTowardsEnemy()
+        public List<Move> MoveTowardsEnemy(List<Move> moves)
         {
-
+            foreach(var unit in MyTeam)
+            {
+                var coord = ClosestEnemyDirection switch
+                {
+                    Direction.NorthEast => unit.Location.MoveNorthEast(1),
+                    Direction.East => unit.Location.MoveEast(1),
+                    Direction.SouthEast => unit.Location.MoveSouthEast(1),
+                    Direction.SouthWest => unit.Location.MoveSouthWest(1),
+                    Direction.West => unit.Location.MoveWest(1),
+                    Direction.NorthWest => unit.Location.MoveNorthWest(1),
+                    _ => unit.Location
+                };
+                moves.Add(new Move(MoveType.Walk, unit.Id, coord));
+            }
+            return moves;
         }
     }
     public enum Direction { NorthEast, East, SouthEast, SouthWest, West, NorthWest }
